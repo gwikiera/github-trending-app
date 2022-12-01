1af30ad50f8d89a09ee2cb48506d969c67377266
Grzegorz Wikiera <gwikiera@gmail.com>
2022-12-01 13:43:32 +0100

    Update Trending/AppRoot/World.swift

--- Trending/AppRoot/World.swift
+++ Trending/AppRoot/World.swift
@@ -16,7 +16,7 @@ var Current = World() // swiftlint:disable:this identifier_name
 private var defaultGitHubAPIClient: GitHubAPIClient {
 #if OFFICIAL_API
     return OfficialGitHubAPIClient(url: Bundle.main.baseURL, apiClient: LiveAPIClient())
-#elseif  UNOFFICIAL_API
+#elseif UNOFFICIAL_API
     return UnofficialGitHubAPIClient(url: Bundle.main.baseURL, apiClient: LiveAPIClient())
 #elseif MOCK_API
     fatalError("Not implemented yet :(.")
