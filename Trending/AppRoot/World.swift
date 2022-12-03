559299fd024a67e8c643492e31e9514883c38cb8
Grzegorz Wikiera <grzegorz_ext@helloclue.com>
2022-12-03 12:52:09 +0100

    Test version of dependencies

--- Trending/AppRoot/World.swift
+++ Trending/AppRoot/World.swift
@@ -18,6 +18,8 @@ var Current = World()
 var Current: World = {
     if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
         return .preview
+    } else if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil {
+        return .unimplemented
     } else {
         return World()
     }
---
+++ Trending/DevelopmentAssets/Unimplemented/ReposRepositoryType+Unimplemented.swift
@@ -0,0 +1,40 @@
+#if DEBUG
+import CombineSchedulers
+import GitHubAPIClient
+import Model
+import XCTestDynamicOverlay
+import Combine
+
+extension World {
+    static var unimplemented: Self {
+        .init(
+            gitHubApiClient: UnimplementedGitHubAPIClient(),
+            reposRepository: UnimplementedReposRepository(),
+            scheduler: .unimplemented
+        )
+    }
+}
+
+private class UnimplementedGitHubAPIClient: GitHubAPIClient {
+    func trendingRepos() async throws -> [Model.Repo] {
+        XCTFail("Unimplemented")
+        return []
+    }
+}
+
+final class UnimplementedReposRepository: ReposRepositoryType {
+    func repos() -> AnyPublisher<[Model.Repo], Never> {
+        XCTFail("Unimplemented")
+        return Empty().eraseToAnyPublisher()
+    }
+
+    func repo(for id: Model.Repo.ID) -> Model.Repo? {
+        XCTFail("Unimplemented")
+        return nil
+    }
+
+    func fetchRepos() async throws {
+        XCTFail("Unimplemented")
+    }
+}
+#endif
