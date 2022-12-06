e9abb47b7ebe05406a8473d92794c24fb6bcc5f5
Grzegorz Wikiera <grzegorz_ext@helloclue.com>
2022-12-06 21:42:20 +0100

    Added `Logger` to `Networking` module

--- Networking/Package.swift
+++ Networking/Package.swift
@@ -12,11 +12,17 @@ let package = Package(
             name: "Networking",
             targets: ["Networking"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Networking"
+            name: "Networking",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log")
+            ]
         ),
         .testTarget(
             name: "NetworkingTests",
--- Networking/Sources/Networking/LiveAPIClient.swift
+++ Networking/Sources/Networking/LiveAPIClient.swift
@@ -1,39 +1,55 @@
 import Foundation
+import Logging
 
 public class LiveAPIClient: APIClient {
     let urlSession: URLSessionProtocol
     let jsonDecoder: JSONDecoder
+    let logger: Logger?
 
-    public convenience init() {
+    public convenience init(logger: Logger? = nil) {
         self.init(
             urlSession: URLSession.shared,
-            jsonDecoder: .init()
+            jsonDecoder: .init(),
+            logger: logger
         )
     }
 
     init(
         urlSession: URLSessionProtocol,
-        jsonDecoder: JSONDecoder
+        jsonDecoder: JSONDecoder,
+        logger: Logger?
     ) {
         self.urlSession = urlSession
         self.jsonDecoder = jsonDecoder
+        self.logger = logger
     }
 
     public func fetch<Model>(_ type: Model.Type, urlRequest: URLRequest) async throws -> Model where Model: Decodable {
+        var logger = logger
+        logger?[metadataKey: "request-hash"] = "\(urlRequest.hashValue)"
+        logger?.debug("Request: \(urlRequest.debugDescription)")
+
         let (data, response): (Data, URLResponse)
         do {
             (data, response) = try await urlSession.data(for: urlRequest)
+            logger?.debug("Response: \(response)")
+            logger?.trace("Data: \(String(describing: String(data: data, encoding: .utf8)))")
         } catch {
+            logger?.error("Error: \(error.localizedDescription)")
             throw APIClientError.networkingError(error)
         }
 
         if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+            logger?.error("Invalid response code: \(String(describing: (response as? HTTPURLResponse)?.statusCode))")
             throw APIClientError.invalidResponseCode
         }
 
         do {
-            return try jsonDecoder.decode(Model.self, from: data)
+            let object = try jsonDecoder.decode(Model.self, from: data)
+            logger?.trace("Decoded object: \(object)")
+            return object
         } catch {
+            logger?.error("Decoding error: \(String(describing: (response as? HTTPURLResponse)?.statusCode))")
             throw APIClientError.decodingError(error)
         }
     }
